1e51e1b8919ab3208f4e601ecf0b553d28f25671
Luanderilidio <luanderilidio174@gmail.com>
2024-08-23 18:02:11 -0400

    feat init

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://dashboard-censo-unemat.com",
+      // Outras opções de configuração
+    }),
+  ],
+});
